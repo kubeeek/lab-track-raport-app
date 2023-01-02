f679570d2c9e0d452e49c7708988d12c0b9bf0ff
Jakub Galecki <jakub.galecki@student.uj.edu.pl>
2023-01-03 00:10:47 +0100

    added export route

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,17 +1,19 @@
+from django.shortcuts import redirect
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
+    path('', lambda request: redirect('testsample_add', permanent=True)),
+
     # ex: /test-sample/add/
     path('test-sample/add', views.TestSampleCreateView.as_view(), name="testsample_add"),
     path('test-sample/', views.TestSampleListView.as_view(), name="testsample_list"),
     path('test-sample/<int:pk>/', views.TestSampleDetailView.as_view(), name="testsample_detail"),
     path('test-sample/<int:pk>/test-label/add', views.TestLabelCreateView.as_view(), name="add_related_testlabel"),
 
+    path('test-sample/export', views.TestSampleExportView.as_view(), name="testsample_export"),
     # ex: /test-label/1/
-    path('test-label/<int:pk>', views.TestLabelDetailView.as_view(), name="testlabel_detail"),
-    path('test-label/<int:pk>/update', views.TestLabelUpdateView.as_view(), name="testlabel_update")
 
     # path('test-label/add', views.TestLabelCreateView.as_view())
 ]
