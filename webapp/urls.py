8dbcf0f11ae76d02256ae3bde3497b60829cb46a
Jakub Galecki <jakub.galecki@student.uj.edu.pl>
2023-01-14 21:08:17 +0100

    refactoring of names to snake case part 1

--- webapp/urls.py
+++ webapp/urls.py
@@ -4,22 +4,22 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', lambda request: redirect('testsample_add', permanent=True)),
+    path('', lambda request: redirect('test_sample_create', permanent=True)),
 
-    # ex: /test-sample/add/
-    path('test-sample/add', views.TestSampleCreateView.as_view(), name="testsample_add"),
-    path('test-sample/', views.TestSampleListView.as_view(), name="testsample_list"),
-    path('test-sample/<int:pk>/', views.TestSampleDetailView.as_view(), name="testsample_detail"),
-    path('test-sample/<int:pk>/update', views.TestSampleUpdateView.as_view(), name="testsample_update"),
-    path('test-sample/<int:pk>/delete', views.TestSampleDeleteView.as_view(), name="testsample_delete"),
+    # ex: /test-sample/create/
+    path('test-sample/create', views.TestSampleCreateView.as_view(), name="test_sample_create"),
+    path('test-sample/', views.TestSampleListView.as_view(), name="test_sample_list"),
+    path('test-sample/<int:pk>/', views.TestSampleDetailView.as_view(), name="test_sample_detail"),
+    path('test-sample/<int:pk>/update', views.TestSampleUpdateView.as_view(), name="test_sample_update"),
+    path('test-sample/<int:pk>/delete', views.TestSampleDeleteView.as_view(), name="test_sample_delete"),
 
-    path('test-sample/<int:pk>/test-label/add', views.TestLabelCreateView.as_view(), name="add_related_testlabel"),
+    path('test-sample/<int:pk>/test-label/add', views.TestLabelCreateView.as_view(), name="add_related_test_label"),
 
-    path('test-sample/export', views.TestSampleExportView.as_view(), name="testsample_export"),
+    path('test-sample/export', views.TestSampleExportView.as_view(), name="test_sample_export"),
     # ex: /test-label/1/
-    path('test-label/<int:pk>/', views.TestLabelDetailView.as_view(), name="testlabel_detail"),
-    path('test-label/<int:pk>/update', views.TestLabelUpdateView.as_view(), name="testlabel_update"),
-    path('test-label/<int:pk>/delete', views.TestLabelDeleteView.as_view(), name="testlabel_delete"),
+    path('test-label/<int:pk>/', views.TestLabelDetailView.as_view(), name="test_label_detail"),
+    path('test-label/<int:pk>/update', views.TestLabelUpdateView.as_view(), name="test_label_update"),
+    path('test-label/<int:pk>/delete', views.TestLabelDeleteView.as_view(), name="test_label_delete"),
 
     # path('test-label/add', views.TestLabelCreateView.as_view())
 ]
