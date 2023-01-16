5137bac2869975dac7575a5e626c892342b53cd6
Jakub Galecki <jakub.galecki@student.uj.edu.pl>
2023-01-16 19:59:41 +0100

    corrected urls

--- webapp/views/TestLabel/TestLabelExportFormView.py
+++ webapp/views/TestLabel/TestLabelExportFormView.py
@@ -7,7 +7,7 @@ class TestLabelExportFormView(ExportFormView):
     def get_context_data(self, **kwargs):
         context = super(TestLabelExportFormView, self).get_context_data(**kwargs)
 
-        context['submit_url'] = reverse('test_label_export_download')
+        context['submit_url'] = reverse('testlabel_export_download')
         context['header'] = "Zlecone badania"
 
         return context
